bd4ce9357b79f4dc3bcb9f5ffba87e21ed80d43d
dongtang <tangbinhdong@gmail.com>
2023-10-04 20:53:00 -0500

    unit test

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/useraction/UserActionTest.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/useraction/UserActionTest.java
@@ -7,11 +7,18 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserActionTest {
@@ -24,7 +31,7 @@ public class UserActionTest {
 
     @BeforeAll
     public static void beforeAll() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().clearDriverCache().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
@@ -44,6 +51,32 @@ public class UserActionTest {
         baseURL = "http://localhost:" + port;
     }
 
+
+    @Test
+    public void testHomePageNotAccessible() {
+        driver.get("http://localhost:" + port + "/home");
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        WebElement homeMarker = null;
+        try {
+            homeMarker = wait.until(webDriver -> webDriver.findElement(By.id("nav-files-tab")));
+        } catch (TimeoutException e) {
+            // Expected
+        }
+        assertNull(homeMarker);
+
+        WebDriverWait wait2 = new WebDriverWait(driver, 3);
+        WebElement marker2 = wait.until(webDriver -> webDriver.findElement(By.id("inputUsername")));
+        assertNotNull(marker2);
+    }
+    @Test
+    public void testSignupPageAccessible() {
+        driver.get("http://localhost:" + port + "/signup");
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        WebElement homeMarker = null;
+        homeMarker = wait.until(webDriver -> webDriver.findElement(By.id("inputUsername")));
+        assertNotNull(homeMarker);
+    }
+
     @Test
     public void testUserSignupLoginAndSubmitMessage() {
         String username = "testuser";
@@ -52,7 +85,7 @@ public class UserActionTest {
         driver.get(baseURL + "/signup");
 
         SignUpPage signupPage = new SignUpPage(driver);
-        signupPage.signup("Peter", "Zastoupil", username, password);
+        signupPage.signup("First Name", "Last Name", username, password);
 
         driver.get(baseURL + "/login");
 
